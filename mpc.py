c6adb35134788c03c30778ef9113cea1a6e9cca7
Katherine Li <katherine.li@berkeley.edu>
2019-02-01 16:27:32 -0800

    sequential sampling

--- mpc.py
+++ mpc.py
@@ -6,5 +6,11 @@ num_objects=5
 path="/Users/katherineli/Desktop/MultiPointPushing"
 test = SingulationEnv()
 test.create_random_env(num_objects)
-data_sum = test.sequential_prune_planning_recursive(prune_method=com_only, max_step=3, metric="count threshold", sum_path=path)
-print(data_sum)
+num_trials = 100
+num_steps = 3
+for i in range(num_steps):
+    best_result, best_action = test.best_sequential_sample(num_trials, com_only, max_step=3, metric="count threshold",
+                                                           sum_path=path)
+    test.step(best_action[0], best_action[1],"/Users/katherineli/Desktop/MultiPointPushing/data/sequential",display=True)
+print("result", best_result)
+print("actions", best_action)
