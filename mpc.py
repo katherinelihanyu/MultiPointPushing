5c364e80814aa862a3dced4397103598b95ba41d
Katherine Li <katherine.li@berkeley.edu>
2019-02-02 22:32:24 -0800

    create directory in step if not exist

--- mpc.py
+++ mpc.py
@@ -11,6 +11,7 @@ num_steps = 3
 for i in range(num_steps):
     best_result, best_action = test.best_sequential_sample(num_trials, com_only, max_step=3, metric="count threshold",
                                                            sum_path=path)
-    test.step(best_action[0], best_action[1],"./data/sequential",display=True)
+    test.step(best_action[0], best_action[1], "./data/sequential_%d"%i, display=True)
 print("result", best_result)
 print("actions", best_action)
+
