957f812e645905099b81c74c1b4f2afe9da2a7f4
Katherine Li <katherine.li@berkeley.edu>
2019-04-15 16:28:34 -0700

    test

--- test_state.py
+++ test_state.py
@@ -28,46 +28,46 @@ class TestState(unittest.TestCase):
     #     env.sample(num_steps=3, prune_method=no_prune, metric=env.count_soft_threshold)
     #     np.testing.assert_array_equal(before_summary, env.save())
 
-    def test_sampling_reproducible_same_env(self):
-        num_steps = 2
-        test_env = State()
-        test_env.create_random_env(num_objs=2)
-        before_summary = test_env.save()
-        final_score, actions, final_state = test_env.sample(num_steps=num_steps, prune_method=no_prune, metric=test_env.count_soft_threshold, display=True, path="./sampling")
-        np.testing.assert_array_equal(before_summary, test_env.save())
+#     def test_sampling_reproducible_same_env(self):
+#         num_steps = 2
+#         test_env = State()
+#         test_env.create_random_env(num_objs=2)
+#         before_summary = test_env.save()
+#         final_score, actions, final_state = test_env.sample(num_steps=num_steps, prune_method=no_prune, metric=test_env.count_soft_threshold, display=True, path="./sampling")
+#         np.testing.assert_array_equal(before_summary, test_env.save())
+#         for i in range(num_steps):
+#             start_pt = np.array([actions[i*4], actions[i*4+1]])
+#             end_pt = np.array([actions[i*4+2], actions[i*4+3]])
+#             test_env.push(start_pt, end_pt, display=True, path="./push" + str(i))
+#         print("np.max(final_state - env.save())", np.max(final_state - test_env.save()))
+#         print("final_state - env.save()")
+#         print(final_state - test_env.save())
+#         print("final state")
+#         print(final_state)
+#         print("env.save()")
+#         print(test_env.save())
+#         print("final_score - test_env.count_soft_threshold()", final_score - test_env.count_soft_threshold())
+#         print("final_score", final_score)
+#         print("test_env.count_soft_threshold()", test_env.count_soft_threshold())
+#         np.testing.assert_allclose(final_state, test_env.save())
+#         self.assertEqual(final_score, test_env.count_soft_threshold())
+
+    def test_sampling_reproducible_diff_env(self):
+        num_steps = 3
+        env1 = State()
+        env1.create_random_env(num_objs=10)
+        before_summary = env1.save()
+        env2 = env1.copy()
+        np.testing.assert_array_equal(before_summary, env2.save())
+        final_score, actions, final_state = env1.sample(num_steps=num_steps, prune_method=no_prune, metric=env1.count_soft_threshold, display=False, path="./sampling")
+        np.testing.assert_array_equal(before_summary, env1.save())
+        np.testing.assert_array_equal(before_summary, env2.save())
         for i in range(num_steps):
             start_pt = np.array([actions[i*4], actions[i*4+1]])
             end_pt = np.array([actions[i*4+2], actions[i*4+3]])
-            test_env.push(start_pt, end_pt, display=True, path="./push" + str(i))
-        print("np.max(final_state - env.save())", np.max(final_state - test_env.save()))
-        print("final_state - env.save()")
-        print(final_state - test_env.save())
-        print("final state")
-        print(final_state)
-        print("env.save()")
-        print(test_env.save())
-        print("final_score - test_env.count_soft_threshold()", final_score - test_env.count_soft_threshold())
-        print("final_score", final_score)
-        print("test_env.count_soft_threshold()", test_env.count_soft_threshold())
-        np.testing.assert_allclose(final_state, test_env.save())
-        self.assertEqual(final_score, test_env.count_soft_threshold())
-
-    # def test_sampling_reproducible_diff_env(self):
-    #     num_steps = 3
-    #     env1 = State()
-    #     env1.create_random_env(num_objs=10)
-    #     before_summary = env1.save()
-    #     env2 = env1.copy()
-    #     np.testing.assert_array_equal(before_summary, env2.save())
-    #     final_score, actions, final_state = env1.sample(num_steps=num_steps, prune_method=no_prune, metric=env1.count_soft_threshold, display=False, path="./sampling")
-    #     np.testing.assert_array_equal(before_summary, env1.save())
-    #     np.testing.assert_array_equal(before_summary, env2.save())
-    #     for i in range(num_steps):
-    #         start_pt = np.array([actions[i*4], actions[i*4+1]])
-    #         end_pt = np.array([actions[i*4+2], actions[i*4+3]])
-    #         env2.push(start_pt, end_pt, display=False, path="./push" + str(i))
-    #     np.testing.assert_allclose(final_state, env2.save())
-    #     self.assertEqual(final_score, env2.count_soft_threshold())
+            env2.push(start_pt, end_pt, display=False, path="./push" + str(i))
+        np.testing.assert_allclose(final_state, env2.save())
+        self.assertEqual(final_score, env2.count_soft_threshold())
 
 
 if __name__ == '__main__':
