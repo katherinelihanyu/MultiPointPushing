c1629b141da1a934b91f7bbf9cda33a88e3c18fc
Katherine Li <katherine.li@berkeley.edu>
2019-04-23 23:42:06 -0700

    sample

--- algorithms.py
+++ algorithms.py
@@ -12,6 +12,7 @@ def create_initial_envs(num_heaps, num_objects, data_path):
         env.create_random_env(num_objs=num_objects)
         env.save(path=path)
 
+
 def run_experiments(num_heaps, data_path):
     lst = []
     for i in range(num_heaps):
@@ -19,14 +20,18 @@ def run_experiments(num_heaps, data_path):
         lst.append(result)
     return lst
 
+
 def run_heap(data_path, heap_num):
     path_i =  os.path.join(data_path, "env%d.npy" % heap_num)
     info = np.load(path_i)
     num_objects = int(info[-1])
     env = State(summary=info, num_objs=num_objects)
-    result, actions = env.greedy(num_steps=3, prune_method=no_prune, metric=env.count_soft_threshold)
+    # result, actions = env.greedy(num_steps=3, prune_method=no_prune, metric=env.count_soft_threshold)
+    result, actions, state = env.sample_best(num_sample=200, sample_func=lambda sample_env, sampled: sample_env.sample(
+        num_steps=3, prune_method=no_prune, metric=sample_env.count_soft_threshold, sampled=sampled))
     print("heap%d: %s" % (heap_num, result))
     return result
+
 
 if __name__ == "__main__":
     returns = run_experiments(num_heaps=50, data_path="/nfs/diskstation/katherineli/states")
