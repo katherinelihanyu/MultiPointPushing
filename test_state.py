4f6684ffa4a58c9094af763ef652c19324dd0e1b
Katherine Li <katherine.li@berkeley.edu>
2019-04-30 16:41:23 -0700

    testing

--- test_state.py
+++ test_state.py
@@ -2,9 +2,9 @@ import numpy as np
 import unittest
 from state import *
 
-NUM_OBJS = 10
+NUM_OBJS = 5
 NUM_STEPS = 3
-NUM_SAMPLES = 3
+NUM_SAMPLES = 1
 
 
 # class TestPolygon(unittest.TestCase):
@@ -81,19 +81,23 @@ class TestState(unittest.TestCase):
     #     self.assertEqual(final_score_sample, test_env.count_soft_threshold())
 
     def test_sampling_reproducible_diff_env(self):
-        env1 = State()
-        env1.create_random_env(num_objs=NUM_OBJS)
-        before_summary = env1.save_positions()
-        env2 = env1.copy()
-        np.testing.assert_array_equal(before_summary, env2.save_positions())
-        final_score, actions, final_state, first_step_return, first_step_end_state = env1.sample(num_steps=NUM_STEPS, prune_method=no_prune, metric=env1.count_soft_threshold, sampled=set())
-        np.testing.assert_array_equal(before_summary, env1.save_positions())
-        np.testing.assert_array_equal(before_summary, env2.save_positions())
-        for i in range(NUM_STEPS):
-            action = (np.array([actions[i*4], actions[i*4+1]]), np.array([actions[i*4+2], actions[i*4+3]]))
-            env2.push(action)
-        np.testing.assert_allclose(final_state, env2.save_positions(), err_msg="%s \n %s" % (final_state, env2.save_positions()))
-        np.testing.assert_almost_equal(final_score, env2.count_soft_threshold())
+        for it in range(100):
+            env1 = State()
+            env1.create_random_env(num_objs=NUM_OBJS)
+            before_summary = env1.save_positions()
+            env2 = env1.copy()
+            np.testing.assert_array_equal(before_summary, env2.save_positions())
+            final_score, actions, final_state, first_step_return, first_step_end_state = env1.sample(num_steps=NUM_STEPS, prune_method=no_prune, metric=env1.count_soft_threshold, sampled=set())
+            np.testing.assert_array_equal(before_summary, env1.save_positions())
+            np.testing.assert_array_equal(before_summary, env2.save_positions())
+            for i in range(NUM_STEPS):
+                action = (np.array([actions[i*4], actions[i*4+1]]), np.array([actions[i*4+2], actions[i*4+3]]))
+                env2.push(action)
+                if i == 0:
+                    np.testing.assert_allclose(first_step_end_state, env2.save_positions(), err_msg="Iteration %d First step \n %s \n %s" % (it, first_step_end_state, env2.save_positions()))
+                    np.testing.assert_almost_equal(first_step_return, env2.count_soft_threshold())
+            np.testing.assert_allclose(final_state, env2.save_positions(), err_msg="iteration %d Final \n %s \n %s" % (it, final_state, env2.save_positions()))
+            np.testing.assert_almost_equal(final_score, env2.count_soft_threshold())
     
     def test_best_sample_reproducible(self):
         env = State()
@@ -110,16 +114,17 @@ class TestState(unittest.TestCase):
         self.assertEqual(best_score, env.count_soft_threshold())
 
     def test_greedy_step_reproducible(self):
-        test_env = State()
-        test_env.create_random_env(num_objs=NUM_OBJS)
-        starting_state = test_env.save_positions()
-        starting_score = test_env.count_soft_threshold()
-        best_result, best_push, best_state = test_env.greedy_step(no_prune, test_env.count_soft_threshold)
-        np.testing.assert_array_equal(starting_state, test_env.save_positions())
-        self.assertEqual(starting_score, test_env.count_soft_threshold())
-        test_env.push(best_push, path="push", display=False)
-        np.testing.assert_allclose(best_state, test_env.save_positions(), err_msg="%s \n %s" % (best_state, test_env.save_positions()))
-        np.testing.assert_almost_equal(best_result, test_env.count_soft_threshold())
+        for i in range(20):
+            test_env = State()
+            test_env.create_random_env(num_objs=NUM_OBJS)
+            starting_state = test_env.save_positions()
+            starting_score = test_env.count_soft_threshold()
+            best_result, best_push, best_state = test_env.greedy_step(no_prune, test_env.count_soft_threshold)
+            np.testing.assert_array_equal(starting_state, test_env.save_positions())
+            self.assertEqual(starting_score, test_env.count_soft_threshold())
+            test_env.push(best_push, path="push", display=False)
+            np.testing.assert_allclose(best_state, test_env.save_positions(), err_msg="iteration %d \n %s \n %s" % (i, best_state, test_env.save_positions()))
+            np.testing.assert_almost_equal(best_result, test_env.count_soft_threshold())
 
 
 if __name__ == '__main__':
